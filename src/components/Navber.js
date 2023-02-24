584037e0705ebf8074dbb0a842bdcfd41815b076
ariful171042 <arif171042@gmail.com>
2023-02-24 15:52:55 +0600

    wace fly project start

---
+++ src/components/Navber.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navber = () => {
+  return (
+    <div className="navber flex justify-between container mx-auto mt-40">
+      <div className="logo"> </div>
+    </div>
+  );
+};
+
+export default Navber;
