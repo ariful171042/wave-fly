9a4919bc758eb1736e852a4b03f650d2ba21edc3
ariful171042 <arif171042@gmail.com>
2023-02-27 08:38:49 +0600

    navber done

---
+++ src/components/AllPackages.js
@@ -0,0 +1,5 @@
+const AllPackages = () => {
+  return <div>AllPackages</div>;
+};
+
+export default AllPackages;
--- src/components/Navber.js
+++ src/components/Navber.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { HashLink } from "react-router-hash-link";
 
 const Navber = () => {
   return (
-    <div className="navber flex justify-between container mx-auto mt-40">
-      <div className="logo"> </div>
+    <div className="navber flex w-full items-center h-12 bg-white-500">
+      <div className="nav flex justify-between container items-center mx-auto">
+        <div className="logo text-xl">
+          <HashLink smooth to={"#home"}>
+            WaveFly
+          </HashLink>
+        </div>
+        <ul className="links flex justify-end gap-10 uppercase text-gray-700 ">
+          <li>
+            <HashLink
+              to={"#home"}
+              className="link-item hover:text-red-500 duration-300"
+            >
+              Home
+            </HashLink>
+          </li>
+          <li>
+            <HashLink
+              to={"#home"}
+              className="link-item hover:text-red-500 duration-300"
+            >
+              All Packages
+            </HashLink>
+          </li>
+          <li>
+            <HashLink
+              to={"#home"}
+              className="link-item hover:text-red-500 duration-300"
+            >
+              Contact
+            </HashLink>
+          </li>
+          <li>
+            <HashLink
+              to={"#home"}
+              className="link-item hover:text-red-500 duration-300"
+            >
+              Support
+            </HashLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
